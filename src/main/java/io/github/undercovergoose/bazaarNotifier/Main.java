0a338869459510a21fcb3541e2c0d9e094f295d3
goose <goose@fgi.gov>
2023-07-31 15:06:52 -0400

    updated internal modid

--- src/main/java/io/github/undercovergoose/bazaarNotifier/Main.java
+++ src/main/java/io/github/undercovergoose/bazaarNotifier/Main.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "betterBazaarNotifierThatActuallyWorks", version = "1.0.0")
+@Mod(modid = "bazaarNotifier", version = "1.0.0")
 public class Main {
     public static final Minecraft MC = Minecraft.getMinecraft();
     public static Overlay overlay = new Overlay();
